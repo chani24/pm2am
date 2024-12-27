f91c57f32763e83e94ff86af4f6483567961a2d0
Chidera Ani <achidera20@gmail.com>
2024-12-27 02:44:18 -0500

    fix caching

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=15552000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
